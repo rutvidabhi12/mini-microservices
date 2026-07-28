971766020a5181884cf9a5b59b7336b70f84bbaa
Rutvi Dabhi <rutvidabhi93@gmail.com>
2026-07-28 12:38:04 +0530

    Add all microservices: registry, student, gateway, course

---
+++ course-service/src/main/java/com/campus/course_service/CourseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.campus.course_service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients          // <-- "scan my code for Feign client interfaces"
+public class CourseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseServiceApplication.class, args);
+    }
+}
---
+++ studentservice/src/main/java/com/campus/studentservice/Student.java
@@ -0,0 +1,6 @@
+package com.campus.studentservice;
+
+// A "record" is a short way to make a class that only carries data.
+public record Student(Long id, String name, String email) { 
+
+}
